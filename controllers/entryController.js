5950b4285b1778ae10c3bee52bf05dce4f3fc353
nndeto <n_ndeto@yahoo.com>
2021-08-11 14:53:21 -0400

    made more dummy placeholder items in ejs pages

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -6,11 +6,27 @@ const router = express.Router();
 const db = require('../models/index.js');
 
 //Index Route --> needs ejs
+router.get('/', (req, res) => {
+    res.render('entries/entryIndex.ejs')
+})
 
 //New Route --> needs ejs, form, and send form to create route
+router.get('/new', (req, res) => {
+    res.render('entries/entryNew.ejs')
+})
+
 //Show Route --> needs ejs
+router.get('/:entryId', (req, res) => {
+    res.render('entries/entryShow.ejs')
+})
+
 //Create Route
+
 //Edit Route --> needs ejs, form, and sends form to update route
+router.get('/:entryId/edit', (req, res) => {
+    res.render('entries/entryEdit.ejs')
+})
+
 //Update Route --> updated the db data
 //DeleteRoute --> deletes data
 
