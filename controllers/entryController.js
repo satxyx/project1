19b674e66a814df961e8a91ff986e46fccc67271
nndeto <n_ndeto@yahoo.com>
2021-08-11 14:33:07 -0400

    added a controller file and started coding dummy routes

---
+++ controllers/entryController.js
@@ -0,0 +1,17 @@
+//calling express
+const express = require('express');
+const router = express.Router();
+
+//stimulates mongodb database
+const db = require('../models/index.js');
+
+//Index Route --> needs ejs
+
+//New Route --> needs ejs, form, and send form to create route
+//Show Route --> needs ejs
+//Create Route
+//Edit Route --> needs ejs, form, and sends form to update route
+//Update Route --> updated the db data
+//DeleteRoute --> deletes data
+
+module.exports = router;
