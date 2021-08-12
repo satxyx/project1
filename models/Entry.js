a33fbe27501858db7348c0ea3ebfbe9d1bb0445a
nndeto <n_ndeto@yahoo.com>
2021-08-12 19:29:18 -0400

    updated model to accomodate

--- models/Entry.js
+++ models/Entry.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 // Schema  -- > ADD BACK IN USER require later
 const entrySchema = new mongoose.Schema({
     date: { type: Date, required: true }, //change to date once we have date HTML set up
-    user: { type: String}, //change to object once we have user model
+    user: { type: mongoose.Schema.Types.ObjectId,
+            ref:''
+        },
     title: { type: String, required: true},
     content: { type: String, required: true},
     image: { type: String, required: true } //change once we figure out how to upload IMGs
