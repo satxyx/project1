a1869cdfae395762c75d807b165aff71b48f1fdb
nndeto <n_ndeto@yahoo.com>
2021-08-11 22:54:15 -0400

    coded create user ejs page

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,4 +4,28 @@ const router = express.Router();
 
 const db = require('../models/index.js');
 
+//Index Route
+router.get ('/', (req, res) => {
+    res.send("you found me")
+})
+//New Route
+router.get('/new', (req, res) => {
+    res.send("you made it to create")
+})
+//Create route
+
+//Show Route
+router.get('/:userId', (req, res) => {
+    res.send("See a single one of me")
+})
+
+//Edit Route
+router.get('/:userId/edit', (req, res) => {
+    res.send("edit me")
+})
+
+//Update Route
+
+//Delete Route
+
 module.exports = router;
