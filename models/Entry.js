de0443ba3d15c513ca88195531ec018e14ccc23a
nndeto <n_ndeto@yahoo.com>
2021-08-13 02:21:32 -0400

    styled entry show page

--- models/Entry.js
+++ models/Entry.js
@@ -3,12 +3,10 @@ const mongoose = require('mongoose')
 // Schema  -- > ADD BACK IN USER require later
 const entrySchema = new mongoose.Schema({
     date: { type: Date, required: true }, //change to date once we have date HTML set up
-    user: { type: mongoose.Schema.Types.ObjectId,
-            ref:''
-        },
+    user: { type: String},
     title: { type: String, required: true},
     content: { type: String, required: true},
-    image: { type: String, required: true } //change once we figure out how to upload IMGs
+    image: { type: String} //change once we figure out how to upload IMGs
     
 })
 
