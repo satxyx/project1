acc21621af58c514b09ee4d8188138448809fdc7
nndeto <n_ndeto@yahoo.com>
2021-08-15 15:41:31 -0400

    styled entry show/edit pages

--- models/Entry.js
+++ models/Entry.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose')
 // Schema  -- > ADD BACK IN USER require later
 const entrySchema = new mongoose.Schema({
     date: { type: Date, required: true }, //change to date once we have date HTML set up
-    user: { type: String},
     title: { type: String, required: true},
     content: { type: String, required: true},
     user: {
